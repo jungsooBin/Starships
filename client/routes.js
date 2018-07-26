ebf905f8673c727180e91e7787ef41ecebe936f5
Jordan Marcus <jormarcus92@gmail.com>
2018-07-26 15:19:45 -0400

    some small changes

--- client/routes.js
+++ client/routes.js
@@ -24,13 +24,13 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/home" component={UserHome} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
-
             <Route exact path="/starships" component={ShipList} />
             <Route exact path='/starships/:id' component={SingleShipPage}/>
           </Switch>
--- client/store/cart.js
+++ client/store/cart.js
@@ -1,14 +1,24 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 
 export const addToCart = product => ({
   type: ADD_TO_CART,
   payload: product
 });
 
+export const removeFromCart = productId => ({
+  type: 'REMOVE_FROM_CART',
+  payload: productId
+});
+
+//check if remove is correct
+
 const cartReducer = (cartState = [], action) => {
   switch (action.type) {
     case ADD_TO_CART:
       return [...cartState, action.payload];
+    case REMOVE_FROM_CART:
+      return cartState.filter(obj => obj.id !== action.payload);
     default:
       return cartState;
   }
