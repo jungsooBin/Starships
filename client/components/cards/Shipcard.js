6a6513f2a9edd309837739920a33e2bd2a9f5ea3
Prince <prince@Princes-MacBook-Pro.local>
2018-07-26 14:11:33 -0400

    added ship card

--- client/components/cards/Shipcard.js
+++ client/components/cards/Shipcard.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchShips } from '../../store/ship';
-
+require('./style/shipCard.css')
 //Not really sure about this page, but it would be useful, ask team
 
-class ShipCard extends Component {
 
-  componentDidMount() {
-    this.props.fetchShips();
-  }
 
+import React, { Component } from 'react'
+
+export default class Shipcard extends Component {
   render() {
-    const ships = this.props.ships;
+    return (
+    <div className="card">
+        <div className='img-container'>
+        <img src="https://i.stack.imgur.com/l60Hf.png" />
+        </div>
+    <div className="container">
+        <h4 className = 'center'><b>Ship Name</b></h4>
+        <div className='center'>
+        <p>Name: Ship Name </p> 
+        <p>Model: Ship Model </p> 
+        <p>Price: Ship Price </p> 
+        <button className="button button2">Add to Cart</button>
+        </div>
+      </div>
+  </div>
+    )
   }
 }
