4ec367026a900465f0a44603d68dab31bbfd84f5
Jordan Marcus <jormarcus92@gmail.com>
2018-07-26 19:50:34 -0400

    didnt do any login stuff, it was already done. changed some stuff in routes and added routes to user.js

--- server/api/users.js
+++ server/api/users.js
@@ -11,7 +11,38 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email', 'googleId', 'firstName', 'lastName']
     })
     res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+  } catch (error) { next (error) }
+});
+
+router.get('/:userId', async (req, res, next) => {
+  try{
+    const user = await User.findById(req.params.userId)
+    res.json(user)
+  } catch (error) { next (error) }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.sendStatus(201)
+  } catch (error) { next (error) }
+});
+
+router.put('/:userId', async (req,res,next) => {
+  try {
+    const findUser = await User.findById(req.params.userId)
+    const updatedUser = await findUser.update(req.body)
+    res.status(200).json(updatedUser)
+  } catch (error) { next (error) }
+});
+
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    await User.destroy({
+    where: {
+      id: req.params.userId
+    }
+  })
+  res.sendStatus(204)
+  } catch (error) { next (error) }
+});
