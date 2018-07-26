0b636d81274a6050d73545650089f694de6e3f69
Jungsoo Bin <witberrybean@gmail.com>
2018-07-26 16:01:04 -0400

    now review

--- client/store/review.js
+++ client/store/review.js
@@ -1,48 +1,42 @@
 import axios from 'axios';
 
 const GET_REVIEWS = 'GET_REVIEWS';
+const GET_SINGLE_REVIEW = 'GET_SINGLE_REVIEW';
 const DELETE_REVIEW = 'DELETE_REVIEW';
 export const START_LOADING = 'START_LOADING';
 
 //action creators
 export const getReviews = reviews => ({type: GET_REVIEWS, payload: reviews });
-export const deleteShip = Ship => ({ type: DELETE_REVIEW, payload: Ship });
+export const deleteReview = Review => ({ type: DELETE_REVIEW, payload: Review });
+export const getSingleReview = singleReview => ({ type: GET_SINGLE_REVIEW, payload: singleReview });
 export const startLoading = () => ({ type: START_LOADING });
 
 //thunk creator
-export const fetchReviews = () => {
+export const fetchReviews = (shipId) => {
   return async dispatch => {
-  const res = await axios.get('/api/ship');
+  const res = await axios.get(`/api/reviews/${shipId}`);
   dispatch(getReviews(res.data));
   }
 }
 
-export const fetchSingleShip = ShipId => {
-  return async dispatch => {
-    const res = await axios.get(`/api/Ship/${ShipId}`);
-    return dispatch(getSingleShip(res.data));
-  }
-}
-
-//ask team about using singleShip vs filtering ship by id to get a single one
 const initialState = {
-  ships: [],
-  singleShip: {},
+  reviews: [],
+  singleReview: {},
   isLoading: false
 }
 
-const shipReducer = (state = initialState, action) => {
+const reviewReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_SHIPS:
+    case GET_REVIEWS:
       return {
         ...state,
-        ships: action.payload,
+        reviews: action.payload,
         isLoading: false
       };
-    case GET_SINGLE_SHIP:
+    case GET_SINGLE_REVIEW:
       return {
         ...state,
-        singleShip: action.payload,
+        singleReview: action.payload,
         isLoading: false
       };
     case START_LOADING:
@@ -55,4 +49,4 @@ const shipReducer = (state = initialState, action) => {
   }
 }
 
-export default shipReducer;
+export default reviewReducer;
